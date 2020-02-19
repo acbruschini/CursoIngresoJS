39a7dc7d9d5b4c876a41afafb1afe776ea350dbc
Ariel Bruschini <58345345+acbruschini@users.noreply.github.com>
2020-02-18 21:38:57 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,14 +3,40 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
-	var numero;
+	var numero = 0;
+	var maximo;
+	var minimo;
 
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
+		numero = parseInt(prompt ("Coloque el numero"));
+		if (contador == 0) {
+			maximo = numero;
+			minimo = numero;
+
+		} else {
+			
+			if(numero > maximo){
+				maximo = numero;
 	
+			}
+			if(numero < minimo){
+	
+				minimo = numero;
+			}
+
+		}
+		
+		document.getElementById("maximo").value = maximo;
+		document.getElementById("minimo").value = minimo;
+
+		respuesta = prompt ("Quiere continuar: si o no");
+
+		contador++;
+
+
 	}
 
 
