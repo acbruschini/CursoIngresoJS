8c267f8f2210983fc13a2ae177454a064a1936a2
Ariel Bruschini <58345345+acbruschini@users.noreply.github.com>
2020-02-26 22:00:37 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,47 @@
-function mostrar()
-{
+function mostrar() {
+
+    var cantVarones = 0;
+    var totalNotas = 0;
+    var nota;
+    var sexo;
+
+    for (var i = 0; i < 5; i++) {
+        nota = parseInt(prompt("ingrese la nota "));
+
+        while (isNaN(nota) || nota < 0 || nota > 10) {
+            nota = parseInt(prompt("No es una nota ingrese la nota "));
+        }
+
+        sexo = prompt("ingrese el sexo, m o f ");
+
+        while (sexo != "m" && sexo != "f") {
+            sexo = prompt("No es un sexo, por favor ingrese m o f ");
+        }
+
+        totalNotas += nota;
+
+        if (nota < notaMasBaja || i == 0) {
+
+            var notaMasBaja = nota;
+            var sexoMasBaja = sexo;
+
+        }
+
+        if (sexo == "m" && nota >= 6) {
+
+            cantVarones++;
+
+        }
+
+
+    }
+
+    alert("El promedio fue: " + totalNotas/i);
+    alert("La nota mas baja fue: " + notaMasBaja);
+    alert("El sexo de la nota mas baja fue: " + sexoMasBaja);
+    alert ("Cantidad de varones con notas mas o igual a 6: " + cantVarones);
+
 
 }
+
+
